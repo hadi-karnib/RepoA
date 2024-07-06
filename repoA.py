b412a34f7f365f2cf45a48237bb7c7bd1d66b3e3
karniib <hadikarnib03@gmail.com>
2024-07-06 18:52:33 +0300

    added variable detection

---
+++ repoA.py
@@ -0,0 +1,14 @@
+import yaml
+
+def load_config():
+    path = '../config.yaml'
+    with open(path, 'r') as file:
+        config = yaml.safe_load(file)
+    return config
+
+config = load_config()
+
+if config['error_repoA']:
+    print("Error_RepoA is true")
+else:
+    print("Running in RepoA,all is Good")
